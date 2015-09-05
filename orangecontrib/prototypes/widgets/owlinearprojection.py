54a24b0f4702a5949c38f0118280b9598e47a9ff
janezd <janez.demsar@fri.uni-lj.si>
2015-09-05 08:54:04 -0500

    Prototype extension: remove parent from __init__s

--- orangecontrib/prototypes/widgets/owlinearprojection.py
+++ orangecontrib/prototypes/widgets/owlinearprojection.py
@@ -250,8 +250,8 @@ class OWLinearProjection(widget.OWWidget):
 
     want_graph = True
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
 
         self.data = None
         self.projection = None
