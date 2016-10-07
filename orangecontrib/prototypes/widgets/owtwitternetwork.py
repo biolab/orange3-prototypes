b03ee3b7bf07f2513cfed5fa8fb92524aa9afdf1
rokgomiscek <rok.gomiscek@gmail.com>
2016-10-07 11:46:32 +0200

    Fixes

--- orangecontrib/prototypes/widgets/owtwitternetwork.py
+++ orangecontrib/prototypes/widgets/owtwitternetwork.py
@@ -6,16 +6,23 @@ from Orange.widgets import widget, gui
 from PyQt4 import QtGui
 
 class OWTwitterNetwork(widget.OWWidget):
-    name = "Twitter User Network"
-    description = ""
+    name = "Twitter User Graph"
+    description = "Create a graph of Twitter users."
     icon = "icons/Twitter.svg"
     priority = 10
-    outputs = [("Followers", network.Graph), ("Following", network.Graph), ("All", network.Graph)]
+    outputs = [("Followers", network.Graph),
+               ("Following", network.Graph),
+               ("All", network.Graph)]
 
     want_main_area = False
 
+
     def __init__(self):
         super().__init__()
+
+        self.n_all = 0
+        self.n_followers = 0
+        self.n_following = 0
 
         # GUI
         box = gui.widgetBox(self.controlArea, "Info")
@@ -28,8 +35,9 @@ class OWTwitterNetwork(widget.OWWidget):
         box.layout().addWidget(QtGui.QLabel("Users:"))
         self.users = QtGui.QTextEdit()
         box.layout().addWidget(self.users)
-        self.infoa = gui.widgetLabel(box, '')
-        self.infob = gui.widgetLabel(box, '')
+        gui.label(box, self, 'Following: %(n_following)d\n'
+                             'Followers: %(n_followers)d\n'
+                             'All: %(n_all)d')
         self.button = gui.button(box, self, "Create graph", self.fetch_users)
 
     def fetch_users(self):
@@ -42,7 +50,7 @@ class OWTwitterNetwork(widget.OWWidget):
 
         followers_graph = nx.Graph()
         following_graph = nx.Graph()
-        all = nx.Graph()
+        all_users = nx.Graph()
 
         users = self.users.toPlainText().split("\n")
 
@@ -50,37 +58,33 @@ class OWTwitterNetwork(widget.OWWidget):
             result = t.users.show(screen_name=user)
             id = result["id"]
             followers_graph.add_node(id)
-            followers = []
             following_graph.add_node(id)
-            following = []
-            all.add_node(id)
+            all_users.add_node(id)
             cursor = -1
             while cursor != 0:
                 response = t.followers.ids(screen_name=user, cursor=cursor)
-                followers.extend(response['ids'])
+                for f_id in response['ids']:
+                    followers_graph.add_edge(id, f_id)
+                    all_users.add_edge(id, f_id)
                 cursor = response['next_cursor']
-            for f_id in followers:
-                followers_graph.add_edge(id, f_id)
-                all.add_edge(id, f_id)
             cursor = -1
             while cursor != 0:
                 response = t.friends.ids(screen_name=user, cursor=cursor)
-                following.extend(response['ids'])
+                for f_id in response['ids']:
+                    following_graph.add_edge(id, f_id)
+                    all_users.add_edge(id, f_id)
                 cursor = response['next_cursor']
-            for f_id in following:
-                following_graph.add_edge(id, f_id)
-                all.add_edge(id, f_id)
 
-        self.infob.setText("Number of nodes: %i" % nx.number_of_nodes(all))
-        all = network.readwrite._wrap(all)
+        all_users = network.readwrite._wrap(all_users)
         followers = network.readwrite._wrap(followers_graph)
         following = network.readwrite._wrap(following_graph)
         self.send("Followers", followers)
         self.send("Following", following)
-        self.send("All", all)
+        self.send("All", all_users)
 
-        self.infoa.setText("Number of followers: %i" % len(followers))
-        self.infob.setText("Number of following: %i" % len(following))
+        self.n_all = len(all_users)
+        self.n_followers = len(followers)
+        self.n_following = len(following)
 
 if __name__=="__main__":
     from PyQt4.QtGui import QApplication
