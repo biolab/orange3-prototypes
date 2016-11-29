56bd6bf24c32ad0eaaf75dadb846201fe39f07cd
Kernc <kerncece@gmail.com>
2016-11-29 16:27:31 +0100

    OWGoogleSheets: update, refactor

--- orangecontrib/prototypes/widgets/owgooglesheets.py
+++ orangecontrib/prototypes/widgets/owgooglesheets.py
@@ -1,36 +1,35 @@
-import re
-from collections import namedtuple
-from datetime import datetime
 import logging
 
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import QTimer, Qt
+from PyQt4.QtGui import QApplication, QComboBox, QLabel
 
+from Orange.util import try_
+from Orange.widgets.utils.itemmodels import PyListModel
 from Orange.widgets import widget, gui, settings
 from Orange.data.table import Table
 
-
-log = logging.getLogger()
-
-
-SHEETS_PATTERN = re.compile(
-    r'(?:https?://)?(?:www\.)?'
-     'docs\.google\.com/spreadsheets/d/'
-     '(?P<workbook_id>[-\w_]+)'
-     '(?:/.*?gid=(?P<sheet_id>\d+).*|.*)?',
-    re.IGNORECASE
-)
+log = logging.getLogger(__name__)
 
 
-def SHEETS_URL(url):
-    match = SHEETS_PATTERN.match(url)
-    workbook, sheet = match.group('workbook_id'), match.group('sheet_id')
-    if not workbook: raise ValueError
-    url = 'https://docs.google.com/spreadsheets/d/{}/export?format=tsv'.format(workbook)
-    if sheet: url += '&gid=' + sheet
-    return url
+class URLComboBox(QComboBox):
 
+    class Model(PyListModel):
+        TitleRole = Qt.UserRole + 1
 
-Sheet = namedtuple('Sheet', ('name', 'url'))
+        def data(self, index, role=Qt.DisplayRole):
+            super_data = super().data(index, role)
+            if role == Qt.DisplayRole:
+                title = super().data(index, self.TitleRole)
+                if title:
+                    return '{} ({})'.format(title, super_data)
+            return super_data
+
+    def __init__(self, parent, model_list, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.setModel(self.Model(iterable=model_list, parent=self))
+
+    def setTitleFor(self, i, title):
+        self.model().setData(self.model().index(i, 0), title, self.Model.TitleRole)
 
 
 class OWGoogleSheets(widget.OWWidget):
@@ -48,11 +47,18 @@ class OWGoogleSheets(widget.OWWidget):
 
     def __init__(self):
         super().__init__()
+        self.table = None
+
+        timer = QTimer(self,
+                       singleShot=True,
+                       timeout=self.load_url)
         hb = gui.widgetBox(self.controlArea, 'Google Sheets', orientation='horizontal')
-        self.combo = combo = QtGui.QComboBox(hb)
-        combo.setEditable(True)
-        combo.setMinimumWidth(300)
-        hb.layout().addWidget(QtGui.QLabel('URL:', hb))
+        self.combo = combo = URLComboBox(
+            hb, self.recent, editable=True, minimumWidth=400,
+            insertPolicy=QComboBox.InsertAtTop,
+            editTextChanged=lambda: timer.start(500),
+            currentIndexChanged=lambda: (timer.stop(), self.load_url()))
+        hb.layout().addWidget(QLabel('URL:', hb))
         hb.layout().addWidget(combo)
         hb.layout().setStretch(1, 2)
         box = gui.widgetBox(self.controlArea, "Info", addSpace=True)
@@ -61,13 +67,7 @@ class OWGoogleSheets(widget.OWWidget):
         self.controlArea.layout().addStretch(1)
         gui.auto_commit(self.controlArea, self, 'autocommit', label='Commit')
 
-        self.set_combo_items()
-        self.table = None
         self.set_info()
-        self.timer = QtCore.QTimer(self)
-        combo.editTextChanged.connect(self.on_combo_textchanged)
-        combo.currentIndexChanged.connect(self.on_combo_activated)
-        combo.currentIndexChanged.emit(0)
 
     def set_combo_items(self):
         self.combo.clear()
@@ -77,75 +77,46 @@ class OWGoogleSheets(widget.OWWidget):
     def commit(self):
         self.send('Data', self.table)
 
-    def on_combo_textchanged(self, text):
-        self.timer.stop()
-        try: url = SHEETS_URL(text)
-        except (ValueError, AttributeError):
-            self.error('Unrecognized URL; should be "docs.google.com/spreadsheets/d/<SHEET-ID>"')
-            return
-        self.error()
-        self.timer = QtCore.QTimer(self)
-        self.timer.setSingleShot(True)
-        self.timer.timeout.connect(lambda: self.on_combo_activated(url=url))
-        self.timer.start(500)
+    class Error(widget.OWWidget.Error):
+        error = widget.Msg("Couldn't load spreadsheet: {}. Ensure correct "
+                           "access permissions; rectangular, top-left-aligned "
+                           "sheet data ...")
 
-    def on_combo_activated(self, index=float('inf'), url=''):
-        self.error()
-        # Index from combobox selection
-        if 0 <= index < len(self.recent):
-            sheet = self.recent.pop(index)
-            self.table = self.retrieve(sheet.url)
-            self.recent.insert(0, sheet)
-        # URL from textchanged event
-        elif url:
-            table = self.table = self.retrieve(url)
-            if not table: return
-            sheet = Sheet(table.name, url)
-            self.recent = [s for s in self.recent if s.url != url]
-            self.recent.insert(0, sheet)
-        else: return
+    def load_url(self):
+        url = self.combo.currentText()
+        if not url:
+            return
+        try:
+            with self.progressBar(3) as progress:
+                progress.advance()
+                table = Table.from_url(url)
+                progress.advance()
+        except Exception as e:
+            log.exception("Couldn't load data from: %s", url)
+            self.Error.error(try_(lambda: e.args[0], ''))
+            self.table = None
+        else:
+            self.Error.clear()
+            self.table = table
+            self.combo.setTitleFor(self.combo.currentIndex(), table.name)
         self.set_info()
         self.commit()
 
-        self.combo.editTextChanged.disconnect(self.on_combo_textchanged)
-        self.combo.currentIndexChanged.disconnect(self.on_combo_activated)
-        self.set_combo_items()
-        self.combo.editTextChanged.connect(self.on_combo_textchanged)
-        self.combo.currentIndexChanged.connect(self.on_combo_activated)
-
     def set_info(self):
         data = self.table
-        if not data:
+        if data is None:
             self.data_info.setText('No spreadsheet loaded.')
             return
-        text = "{} instance(s), {} feature(s), {} meta attribute(s)".format(
-            len(data), len(data.domain.attributes), len(data.domain.metas))
-        try: text += '\nFirst entry: {}\nLast entry: {}'.format(data[0, 'Timestamp'],
-                                                                data[-1, 'Timestamp'])
-        except Exception: pass  # no Timestamp header
+        text = "{}\n\n{} instance(s), {} feature(s), {} meta attribute(s)\n".format(
+            data.name, len(data), len(data.domain.attributes), len(data.domain.metas))
+        text += try_(lambda: '\nFirst entry: {}'
+                             '\nLast entry: {}'.format(data[0, 'Timestamp'],
+                                                       data[-1, 'Timestamp']), '')
         self.data_info.setText(text)
-
-    def retrieve(self, url):
-        if not url: return
-        progress = gui.ProgressBar(self, 10)
-        for i in range(3): progress.advance()
-        try: table = Table.from_url(url)
-        except Exception as e:
-            import traceback
-            log.error(traceback.format_exc())
-            log.error("Couldn't load spreadsheet %s: %s", url, e)
-            self.error("Couldn't load spreadsheet. Ensure correct read permissions; rectangular, top-left aligned sheet data ...")
-            return
-        else:
-            for i in range(7): progress.advance()
-        finally:
-            progress.finish()
-        return table
 
 
 if __name__ == "__main__":
-    a = QtGui.QApplication([])
+    a = QApplication([])
     ow = OWGoogleSheets()
     ow.show()
     a.exec_()
-    ow.saveSettings()
