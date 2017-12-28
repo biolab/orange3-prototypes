98494cd4c43098193386b4ae255e6bd58194a411
janezd <janez.demsar@fri.uni-lj.si>
2017-12-29 00:07:01 +0100

    OWGrep: Docstrings and comments

--- orangecontrib/prototypes/widgets/owgrep.py
+++ orangecontrib/prototypes/widgets/owgrep.py
@@ -25,6 +25,16 @@ class NameContextHandler(ContextHandler):
         return (name == context.name) + \
                (os.path.splitext(name)[1] == os.path.splitext(context.name)[1])
 
+# The data flows through the widget as follows:
+# - open_file set current_file (+ in_view) and calls grep_lines;
+# - grep_lines greps into selected_lines and calls set_out_view and commit
+# - set_out_view shows the grepped lines, with headers in bold
+# - commit constructs a table from selected_lines and outputs it
+#
+# Callbacks enter this at the following points:
+# - combo with recent files and the button open call open_file
+# - all pattern-related controls except the checkbox for header call grep_lines
+# - the checkbox for header calls set_out_view and commit, without re-grepping
 
 class OWGrep(OWWidget, RecentPathsWComboMixin):
     name = "Grep"
@@ -153,6 +163,9 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
         return size
 
     def select_file(self, n=0):
+        """
+        Pull the file with the given index in the list to the top and open it.
+        """
         assert n < len(self.recent_paths)
         super().select_file(n)
         if self.recent_paths:
@@ -162,6 +175,9 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
             self.set_file_list()
 
     def browse_file(self):
+        """
+        Open the file dialog and open the chosen file.
+        """
         start_path = self.last_path() or os.path.expanduser("~")
         filename, _ = QFileDialog.getOpenFileName(
             None, "Open File", start_path, 'All Files (*.*)')
@@ -173,6 +189,13 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
         self.open_file()
 
     def open_file(self):
+        """
+        Open the file returned by `last_path`.
+
+        Sets `current_file`, shows its contents in `in_view` and calls
+        `grep_lines`. This happens even if `current_file` is empty, in which
+        case everything is cleared.
+        """
         self.Error.file_not_found.clear()
         self.current_file = self.last_path()
         text = ""
@@ -187,6 +210,13 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
         self.grep_lines()
 
     def grep_lines(self):
+        """
+        Grep the lines from `current_file` into `selected_lines`.
+        If there is no current file, `selected_lines` is set to `[]`.
+        Finally, it calls `set_out_view` and `commit`.
+
+        Depends on `current_file` and all settings except `has_header_row`.
+        """
         def prepare_re():
             pattern = self.pattern
             if not self.regular_expression:
@@ -218,6 +248,12 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
         self.commit()
 
     def set_out_view(self):
+        """
+        Show the grepped lines in the output view.
+
+        Directly uses `selected_lines`, `has_header_row` and `block_length`;
+        depends on other settings through `selected_lines`.
+        """
         if self.has_header_row:
             text = "\n".join(
                 "\n".join(
@@ -230,15 +266,31 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
             text = "\n".join(self.selected_lines)
         self.out_view.setHtml(self.out_css + "<div>{}</div>".format(text))
 
-    # Controls must not use (the original) commit method as a callback
     def has_header_changed(self):
+        """
+        Callback handler for the header checkbox.
+
+        Calls `set_out_view` to update the view (headers are shown in bold)
+        and `commit`."""
         self.set_out_view()
         self.commit()
 
     def commit(self):
+        """Output the data table.
+
+        All functionality is in a separate function for simpler control flow.
+        """
         self.Outputs.data.send(self._get_data())
 
     def _get_data(self):
+        """
+        Return a `Orange.data.Table` with the data from selected lines.
+
+        If the data has a `header_row` but the reader does not recognize it,
+        the function adds a header and rereads it, and shows a warning.
+
+        If `selected_lines` are empty, the function returns `None`.
+        """
         self.Warning.no_header_row.clear()
         self.Error.block_too_short.clear()
         self.Error.unreadable.clear()
@@ -262,6 +314,15 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
 
     # pylint: disable=broad-except
     def _construct_table(self, with_header_row):
+        """
+        Write the `selected_lines` into a temporary file and read it.
+
+        If `with_header_row` is `True`, the first line in every block is
+        skipped, except for the first block.
+
+        If `with_header_row` is `False`, a header row is added.
+        """
+        assert self.selected_lines
         tempf = NamedTemporaryFile("w", suffix=".csv", delete=False)
         if with_header_row:
             data = self.selected_lines[0] + "\n" + \
@@ -285,6 +346,7 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
             os.remove(tempf.name)
 
     def find_changed(self):
+        """Callback for searchin within the file"""
         if not self.find_text:
             return
         in_view = self.in_view
@@ -300,6 +362,7 @@ class OWGrep(OWWidget, RecentPathsWComboMixin):
                 + in_view.cursorRect(cursor).y() - 20)
 
     def copy_to_clipboard(self):
+        """Overloaded from OWWidget to copy the text from the file"""
         self.in_view.copy()
 
 
