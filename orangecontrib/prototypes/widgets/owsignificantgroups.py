2facbb7ead3d4d69fbf0a14a2e31dc860932349e
Kernc <kerncece@gmail.com>
2018-02-02 14:53:26 +0100

    OWSignificantGroups: don't error if a single grouping var is selected
    Fixes https://github.com/biolab/orange3-prototypes/issues/119

--- orangecontrib/prototypes/widgets/owsignificantgroups.py
+++ orangecontrib/prototypes/widgets/owsignificantgroups.py
@@ -226,6 +226,11 @@ class OWSignificantGroups(widget.OWWidget):
             self.Error.no_class_selected()
             return
 
+        # If listview selection was a single item the list of items is not a list,
+        # but futher below we expect it to be
+        if not isinstance(self.chosen_X, (list, tuple)):
+            self.chosen_X = [self.chosen_X]
+
         self.btn_compute.setEnabled(False)
         yvar = self.data.domain[self.chosen_y]
 
