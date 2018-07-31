c767abe3d80094fbe6b1519c6be1e67d76458253
Vesna Tanko <tankovesna@hotmail.com>
2018-07-31 14:35:10 +0200

    TestOWCorrelations: Fix tests

--- orangecontrib/prototypes/widgets/tests/test_owcorrelations.py
+++ orangecontrib/prototypes/widgets/tests/test_owcorrelations.py
@@ -22,73 +22,78 @@ class TestOWCorrelations(WidgetTest):
 
     def test_input_data_cont(self):
         """Check correlation table for dataset with continuous attributes"""
-        self.send_signal("Data", self.data_cont)
+        self.send_signal(self.widget.Inputs.data, self.data_cont)
         time.sleep(0.1)
         n_attrs = len(self.data_cont.domain.attributes)
+        self.process_events()
         self.assertEqual(self.widget.vizrank.rank_model.columnCount(), 2)
         self.assertEqual(self.widget.vizrank.rank_model.rowCount(),
                          n_attrs * (n_attrs - 1) / 2)
-        self.send_signal("Data", None)
+        self.send_signal(self.widget.Inputs.data, None)
         self.assertEqual(self.widget.vizrank.rank_model.columnCount(), 0)
         self.assertEqual(self.widget.vizrank.rank_model.rowCount(), 0)
 
     def test_input_data_disc(self):
         """Check correlation table for dataset with discrete attributes"""
-        self.send_signal("Data", self.data_disc)
+        self.send_signal(self.widget.Inputs.data, self.data_disc)
         self.assertTrue(self.widget.Information.not_enough_vars.is_shown())
-        self.send_signal("Data", None)
+        self.send_signal(self.widget.Inputs.data, None)
         self.assertFalse(self.widget.Information.not_enough_vars.is_shown())
 
     def test_input_data_mixed(self):
         """Check correlation table for dataset with continuous and discrete
         attributes"""
-        self.send_signal("Data", self.data_mixed)
+        self.send_signal(self.widget.Inputs.data, self.data_mixed)
         domain = self.data_mixed.domain
         n_attrs = len([a for a in domain.attributes if a.is_continuous])
         time.sleep(0.1)
+        self.process_events()
         self.assertEqual(self.widget.vizrank.rank_model.columnCount(), 2)
         self.assertEqual(self.widget.vizrank.rank_model.rowCount(),
                          n_attrs * (n_attrs - 1) / 2)
 
     def test_input_data_one_feature(self):
         """Check correlation table for dataset with one attribute"""
-        self.send_signal("Data", self.data_cont[:, [0, 4]])
+        self.send_signal(self.widget.Inputs.data, self.data_cont[:, [0, 4]])
         self.assertEqual(self.widget.vizrank.rank_model.columnCount(), 0)
         self.assertTrue(self.widget.Information.not_enough_vars.is_shown())
-        self.send_signal("Data", None)
+        self.send_signal(self.widget.Inputs.data, None)
         self.assertFalse(self.widget.Information.not_enough_vars.is_shown())
 
     def test_input_data_one_instance(self):
         """Check correlation table for dataset with one instance"""
-        self.send_signal("Data", self.data_cont[:1])
+        self.send_signal(self.widget.Inputs.data, self.data_cont[:1])
         self.assertEqual(self.widget.vizrank.rank_model.columnCount(), 0)
         self.assertTrue(self.widget.Information.not_enough_inst.is_shown())
-        self.send_signal("Data", None)
+        self.send_signal(self.widget.Inputs.data, None)
         self.assertFalse(self.widget.Information.not_enough_inst.is_shown())
 
     def test_output_data(self):
         """Check dataset on output"""
-        self.send_signal("Data", self.data_cont)
+        self.send_signal(self.widget.Inputs.data, self.data_cont)
         time.sleep(0.1)
+        self.process_events()
         self.widget.commit()
-        self.assertEqual(self.data_cont, self.get_output("Data"))
+        self.assertEqual(self.data_cont, self.get_output(self.widget.Outputs.data))
 
     def test_output_features(self):
         """Check features on output"""
-        self.send_signal("Data", self.data_cont)
+        self.send_signal(self.widget.Inputs.data, self.data_cont)
         time.sleep(0.1)
+        self.process_events()
         attrs = self.widget.cont_data.domain.attributes
         self.widget._vizrank_selection_changed(attrs[0], attrs[1])
-        features = self.get_output("Features")
+        features = self.get_output(self.widget.Outputs.features)
         self.assertIsInstance(features, AttributeList)
         self.assertEqual(len(features), 2)
 
     def test_output_correlations(self):
         """Check correlation table on on output"""
-        self.send_signal("Data", self.data_cont)
+        self.send_signal(self.widget.Inputs.data, self.data_cont)
         time.sleep(0.1)
+        self.process_events()
         self.widget.commit()
-        correlations = self.get_output("Correlations")
+        correlations = self.get_output(self.widget.Outputs.correlations)
         self.assertIsInstance(correlations, Table)
         self.assertEqual(len(correlations), 6)
         self.assertEqual(len(correlations.domain.attributes), 1)
@@ -96,11 +101,13 @@ class TestOWCorrelations(WidgetTest):
 
     def test_scatterplot_input_features(self):
         """Check if attributes have been set after sent to scatterplot"""
-        self.send_signal("Data", self.data_cont)
+        self.send_signal(self.widget.Inputs.data, self.data_cont)
         scatterplot_widget = self.create_widget(OWScatterPlot)
-        features = self.get_output("Features")
-        self.send_signal("Data", self.data_cont, widget=scatterplot_widget)
-        self.send_signal("Features", features, widget=scatterplot_widget)
+        attrs = self.widget.cont_data.domain.attributes
+        self.widget._vizrank_selection_changed(attrs[2], attrs[3])
+        features = self.get_output(self.widget.Outputs.features)
+        self.send_signal(self.widget.Inputs.data, self.data_cont, widget=scatterplot_widget)
+        self.send_signal(scatterplot_widget.Inputs.features, features, widget=scatterplot_widget)
         self.assertIs(scatterplot_widget.attr_x, self.data_cont.domain[2])
         self.assertIs(scatterplot_widget.attr_y, self.data_cont.domain[3])
 
