92ee13968af52d47df67d4f3d26956a0784eeeb5
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2018-10-10 15:03:25 +0200

    owcsvimport: Add test for load_csv

--- orangecontrib/prototypes/widgets/tests/test_owcsvimport.py
+++ orangecontrib/prototypes/widgets/tests/test_owcsvimport.py
@@ -1,5 +1,10 @@
-import os
 import unittest
+
+import os
+import io
+import csv
+
+import numpy as np
 
 from Orange.widgets.tests.base import WidgetTest, GuiTest
 
@@ -50,3 +55,57 @@ class TestImportDialog(GuiTest):
         opts1 = d.options()
         d.reset()
         opts1 = d.options()
+
+
+class TestUtils(unittest.TestCase):
+    def test_load_csv(self):
+        contents = (
+            b'1/1/1990,1.0,[,one,\n'
+            b'1/1/1990,2.0,],two,\n'
+            b'1/1/1990,3.0,{,three,'
+        )
+        ColumnType = owcsvimport.Options.ColumnType
+        RowSpec = owcsvimport.Options.RowSpec
+        opts = owcsvimport.Options(
+            encoding="ascii",
+            dialect=csv.excel(),
+            columntypes=[
+                (range(0, 1), ColumnType.Time),
+                (range(1, 2), ColumnType.Numeric),
+                (range(2, 3), ColumnType.Text),
+                (range(3, 4), ColumnType.Categorical),
+            ],
+            rowspec=[]
+        )
+        df = owcsvimport.load_csv(io.BytesIO(contents), opts)
+        self.assertEqual(df.shape, (3, 5))
+        self.assertSequenceEqual(
+            list(df.dtypes),
+            [np.dtype("M8[ns]"), np.dtype(float), np.dtype(object),
+             "category", np.dtype(float)],
+        )
+        opts = owcsvimport.Options(
+            encoding="ascii",
+            dialect=csv.excel(),
+            columntypes=[
+                (range(0, 1), ColumnType.Skip),
+                (range(1, 2), ColumnType.Numeric),
+                (range(2, 3), ColumnType.Skip),
+                (range(3, 4), ColumnType.Categorical),
+                (range(4, 5), ColumnType.Skip),
+            ],
+            rowspec=[
+                (range(1, 2), RowSpec.Skipped)
+            ]
+        )
+        df = owcsvimport.load_csv(io.BytesIO(contents), opts)
+        self.assertEqual(df.shape, (2, 2))
+        self.assertSequenceEqual(
+            list(df.dtypes), [np.dtype(float), "category"]
+        )
+        self.assertSequenceEqual(
+            list(df.iloc[:, 0]), [1.0, 3.0]
+        )
+        self.assertSequenceEqual(
+            list(df.iloc[:, 1]), ["one", "three"]
+        )
