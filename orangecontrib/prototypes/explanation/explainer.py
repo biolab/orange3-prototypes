bd3467581d95271b0722106cdd0222c87f2035eb
Primož Godec <p.godec9@gmail.com>
2020-04-30 13:20:19 +0200

    Explainer: Fix prediction on data with different class

--- orangecontrib/prototypes/explanation/explainer.py
+++ orangecontrib/prototypes/explanation/explainer.py
@@ -9,7 +9,7 @@ from sklearn.cluster import KMeans
 from sklearn.impute import SimpleImputer
 
 from Orange.base import Model
-from Orange.data import Table
+from Orange.data import Table, Domain
 from Orange.util import dummy_callback, wrap_callback
 from shap import KernelExplainer, TreeExplainer
 from shap.common import DenseData, SHAPError, sample
@@ -442,8 +442,13 @@ def explain_predictions(
         progress_callback = dummy_callback
     progress_callback(0)
 
+    # prediction happens independent from the class -
+    # same than for prediction widget
+    classless_data = data.transform(
+        Domain(data.domain.attributes, None, data.domain.metas)
+    )
     predictions = model(
-        data, model.Probs if model.domain.class_var.is_discrete else model.Value
+        classless_data, model.Probs if model.domain.class_var.is_discrete else model.Value
     )
     # for regression - predictions array is 1d transform it shape N x 1
     if predictions.ndim == 1:
