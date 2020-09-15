fa1a0456a1b0f78d70c3ef94f421229d9826d2ea
Primož Godec <p.godec9@gmail.com>
2020-09-15 14:58:02 +0200

    Improvement of create table widget

--- orangecontrib/prototypes/widgets/owcreatetable.py
+++ orangecontrib/prototypes/widgets/owcreatetable.py
@@ -1,20 +1,26 @@
 from typing import Optional
 
+import numpy as np
 from AnyQt.QtCore import Qt, QAbstractTableModel, QModelIndex, QSize
 from AnyQt.QtWidgets import QTableView, QItemDelegate, QLineEdit, QCompleter
 
-from Orange.data.table import Table, Domain, ContinuousVariable, \
-    DiscreteVariable
+from Orange.data import (
+    Table, Domain, ContinuousVariable, DiscreteVariable, TimeVariable
+)
 from Orange.widgets import gui
-from Orange.widgets.settings import Setting
+from Orange.widgets.settings import (
+    Setting, ContextSetting, PerfectDomainContextHandler
+)
 from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.widgets.widget import OWWidget, Output, Input, Msg
+
+
+DEFAULT_DATA = [[None for x in range(3)] for y in range(3)]
 
 
 class EditableTableItemDelegate(QItemDelegate):
     def createEditor(self, parent, options, index: QModelIndex):
         model = index.model()  # type: EditableTableModel
-
         if not model.is_discrete(index.column()):
             return super().createEditor(parent, options, index)
 
@@ -28,6 +34,10 @@ class EditableTableItemDelegate(QItemDelegate):
 
         edit.editingFinished.connect(save)
         return edit
+
+    def setEditorData(self, editor, index):
+        current_val = index.model().data(index, Qt.DisplayRole)
+        editor.setText(current_val)
 
 
 class EditableTableModel(QAbstractTableModel):
@@ -44,8 +54,30 @@ class EditableTableModel(QAbstractTableModel):
 
     def is_discrete(self, column):
         column_data = set(row[column] for row in self._table) - {None}
-        return (self._domain is not None and self._domain[column].is_discrete) or \
-               (column_data and all(map(lambda s: isinstance(s, str), column_data)))
+        return (
+            self._domain is not None and self._domain[column].is_discrete
+            or (
+                column_data
+                and not self.is_time_variable(column)
+                and not all(map(lambda s: s.isdigit(), column_data))
+            )
+        )
+
+    def is_time_variable(self, column):
+        values = self.time_vals(column)
+        return values and not all(e is None for e in values)
+
+    def time_vals(self, column):
+        column_data = [row[column] for row in self._table]
+        try:
+            tvar = TimeVariable('_')
+            values = [
+                tvar.parse_exact_iso(d) if d is not None
+                else None for d in column_data
+            ]
+            return values
+        except ValueError:
+            return None
 
     def discrete_vals(self, column):
         if self._domain is not None and self._domain[column].is_discrete:
@@ -70,14 +102,20 @@ class EditableTableModel(QAbstractTableModel):
             return Qt.AlignVCenter | Qt.AlignLeft
 
     def setData(self, index: QModelIndex, value: str, role=None):
-        try:
-            value = float(value)
-        except ValueError:
-            pass
         value = None if not value else value
         self._table[index.row()][index.column()] = value
         self.dataChanged.emit(index, index)
         return True
+
+    def set_table(self, table):
+        self._table = table
+        self.dataChanged.emit(
+            self.index(0, 0),
+            self.index(len(table) - 1, len(table[0]))
+        )
+
+    def get_raw_table(self):
+        return self._table
 
     def rows_changed_to(self, n_rows):
         while n_rows != self.rowCount():
@@ -123,13 +161,17 @@ class EditableTableModel(QAbstractTableModel):
                 return self._domain[section].name
 
     def clear(self):
-        self.rows_changed_to(1)
-        self.columns_changed_to(1)
-        self._table[0][0] = None
+        self.rows_changed_to(3)
+        self.columns_changed_to(3)
+        self.set_table([[None for x in range(3)] for y in range(3)])
 
     def get_table(self):
         domain = self.get_domain()
-        return Table.from_list(domain, self._table)
+        data = np.array(self._table)  # type:
+        for ci in range(data.shape[1]):
+            if isinstance(domain.variables[ci], TimeVariable):
+                data[:, ci] = self.time_vals(ci)
+        return Table.from_list(domain, data)
 
     def get_domain(self):
         if self._domain is not None:
@@ -138,9 +180,12 @@ class EditableTableModel(QAbstractTableModel):
         vars = []
         for ci in range(self.columnCount()):
             if self.is_discrete(ci):
-                values = set(row[ci] for row in self._table
-                             if row[ci] is not None)
+                values = set(
+                    row[ci] for row in self._table if row[ci] is not None
+                )
                 var = DiscreteVariable(name=str(ci + 1), values=values)
+            elif self.is_time_variable(ci):
+                var = TimeVariable(name=str(ci + 1))
             else:
                 var = ContinuousVariable(name=str(ci + 1))
             vars.append(var)
@@ -160,11 +205,16 @@ class OWCreateTable(OWWidget):
         data = Output("Data", Table)
 
     class Error(OWWidget.Error):
-        transform_err = Msg("Cannot transform input data into table!")
+        transform_err = Msg("Data does not fit to domain")
 
-    n_rows = Setting(1)
-    n_columns = Setting(1)
+    settingsHandler = PerfectDomainContextHandler()
+
+    n_rows = Setting(len(DEFAULT_DATA))
+    n_columns = Setting(len(DEFAULT_DATA[0]))
     auto_commit = Setting(True)
+    # since data is a small (at most 20x20) table we can afford to store it
+    # as a context
+    data = ContextSetting(DEFAULT_DATA.copy(), schema_only=True)
 
     def __init__(self):
         super().__init__()
@@ -188,6 +238,8 @@ class OWCreateTable(OWWidget):
         self.table_model = EditableTableModel()
         self.table.setModel(self.table_model)
         self.table_model.dataChanged.connect(self.commit)
+        self.rows_changed()
+        self.columns_changed()
 
     def rows_changed(self):
         self.table_model.rows_changed_to(self.n_rows)
@@ -208,20 +260,35 @@ class OWCreateTable(OWWidget):
 
     @Inputs.data
     def set_dataset(self, data):
+        self.closeContext()
+        self.data = DEFAULT_DATA.copy()
         if data is not None:
             self.table_model.set_domain(data.domain)
             self.c_spin.setEnabled(False)
         else:
             self.table_model.set_domain(None)
             self.c_spin.setEnabled(True)
-
         self.r_spin.setValue(self.table_model.rowCount())
         self.c_spin.setValue(self.table_model.columnCount())
+        print(self.data)
+        self.openContext(data)
         self.unconditional_commit()
 
     @staticmethod
     def sizeHint():
         return QSize(800, 500)
+
+    def closeContext(self):
+        self.data = self.table_model.get_raw_table()
+        print("here", DEFAULT_DATA)
+        super(OWCreateTable, self).closeContext()
+
+    def openContext(self, domain):
+        print(self.data)
+        super(OWCreateTable, self).openContext(domain)
+        print(self.data)
+        self.table_model.set_table(self.data)
+        print(self.data)
 
 
 if __name__ == "__main__":  # pragma: no cover
