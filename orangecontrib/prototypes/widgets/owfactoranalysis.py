2b7e29b38dcbc6ce5e9c2e98351614017b288dee
suriikata <iris.cilensek1@gmail.com>
2022-05-28 17:14:31 +0200

    220528

--- orangecontrib/prototypes/widgets/owfactoranalysis.py
+++ orangecontrib/prototypes/widgets/owfactoranalysis.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 from sklearn.decomposition import FactorAnalysis
 
@@ -10,7 +10,20 @@ from Orange.widgets import settings
 from Orange.widgets.widget import OWWidget
 from orangewidget.widget import Input, Output
 from orangewidget.utils.widgetpreview import WidgetPreview
+from Orange.widgets.utils.slidergraph import SliderGraph
 from orangewidget import gui
+
+from pyqtgraph import mkPen, TextItem
+from AnyQt.QtGui import QColor
+
+
+class Rotation:
+    NoRotation, Varimax, Quartimax = 0, 1, 2
+
+    @staticmethod
+    def items():
+        return ["NoRotation", "Varimax", "Quartimax"]
+
 
 class OWFactorAnalysis(OWWidget):
     name = "Factor Analysis"
@@ -24,39 +37,97 @@ class OWFactorAnalysis(OWWidget):
     class Outputs:
         sample = Output("Sampled Data", Table)
 
-    settingsHandler = settings.DomainContextHandler()   # TODO puciga
     n_components = settings.ContextSetting(1)
-    commitOnChange = settings.Setting(0)        #TODO puciga
+    setting_for_rotation = settings.Setting(Rotation.NoRotation)
     autocommit = settings.Setting(True)
 
-    def __init__(self): # __init__ je konstruktor (prva metoda ki se klice, ko se ustvari instance tega razreda)
-        super().__init__()  # Ker je OWFactorAnalysis derivat OWWIdgeta (deduje iz OWWidgeta), najprej inicializiram njega
+    def __init__(self):
+        super().__init__()  # since OWFactorAnalysis is a derivative of OWWidget, first intialize OWFA
         self.dataset = None
-
-        # Control area settings
-        self.optionsBox = gui.widgetBox(self.controlArea, "Options")
-        gui.spin(
-            self.optionsBox,
-            self,
-            "n_components",
-            minv=1,
-            maxv=100,
-            step=1,
-            label="Number of components:",
-            callback=[self.factor_analysis, self.commit.deferred], #deferred = zapoznelo
-        )
-
-        gui.auto_commit(
-            self.controlArea, self, 'autocommit', 'Commit',
-            orientation=Qt.Horizontal)
-
-        gui.separator(self.controlArea)  # TODO tega mogoce ne potrebujem ker je to za zadnjim elementom v controlArea
 
         # Main area settings
         self.mainArea.setVisible(True)
 
-        gui.separator(self.mainArea) # TODO tega mogoce ne potrebujem, ker je to pred prvim elementom v mainArea
+        self.attr_box = gui.hBox(self.mainArea, margin=0)
 
+        gui.spin(
+            self.attr_box, self, "n_components", label="Number of components:",
+            minv=1, maxv=100, step=1, controlWidth=30,
+            callback=[self.factor_analysis, self.commit.deferred],  # deferred = zapoznelo
+        )
+
+        gui.comboBox(
+            self.attr_box, self, "setting_for_rotation", label="Rotation:", labelWidth=50,
+            items=Rotation.items(), orientation=Qt.Horizontal,
+            contentsLength=12, callback=self.factor_analysis
+        )
+
+        gui.auto_commit(
+            self.attr_box, self, 'autocommit', 'Commit',
+            orientation=Qt.Horizontal
+        )
+        gui.separator(self.mainArea)  # do i need it? >>> the first element in mainArea
+
+        self.plot = SliderGraph("Factor 1", "Factor 2", self.prazna_funkcija)
+        self.mainArea.layout().addWidget(self.plot)
+
+    def prazna_funkcija(self):  # bc _init_ Slidergrapha requires "callback"
+        pass
+
+    def get_range(self, factor):
+        max_value = factor[0]
+        for i in range(len(factor)):
+            if factor[i] > max_value:
+                max_value = factor[i]
+
+        min_value = factor[0]
+        for i in range(len(factor)):
+            if factor[i] < min_value:
+                min_value = factor[i]
+
+        # adjust and scale by 0.1
+        min_value = min_value - 0.1 * abs(min_value)
+        max_value = max_value + 0.1 * abs(max_value)
+
+        # return the abs value of maximum
+        return max(abs(min_value), abs(max_value))
+
+    def set_range(self):
+        factor1_range = self.get_range(self.factor1)
+        factor2_range = self.get_range(self.factor2)
+        self.plot.setRange(xRange=(-factor1_range, factor1_range), yRange=(-factor2_range, factor2_range))
+
+    def setup_plot(self):
+        self.plot.clear_plot()
+        if self.n_components == 1:
+            return
+
+        self.factor1 = self.result.X[0]
+        self.factor2 = self.result.X[1]
+
+        self.set_range()
+
+        foreground = self.plot.palette().text().color()
+        foreground.setAlpha(128)
+
+        names = []
+        for i in range(len(self.dataset.domain.attributes)):
+            name = self.dataset.domain.attributes[i].name
+            names.append(name)
+
+        for x, y, n in zip(self.factor1, self.factor2, names):
+            x_vektor, y_vektor = [0, x], [0, y]
+            self.plot.plot(x_vektor, y_vektor, pen=mkPen(QColor(Qt.red), width=1), antialias=True)
+
+            if n is not None:
+                label = TextItem(
+                    text=n, anchor=(0, 1), color=foreground)
+                label.setPos(x_vektor[-1], y_vektor[-1])
+                self.plot.x = x_vektor
+                self.plot._set_anchor(label, len(x_vektor) - 1, True)
+                self.plot.addItem(label)
+
+        """ TABELA TODO: factor loadings po rotaciji 
         box = gui.vBox(self.mainArea, box = "Eigenvalue Scores")
         self.left_side.setContentsMargins(0,0,0,0)
         table = self.table_view = QTableView(self.mainArea)
@@ -70,29 +141,37 @@ class OWFactorAnalysis(OWWidget):
         table.horizontalHeader().hide()
         table.setShowGrid(False)
         box.layout().addWidget(table)
-
+        """
 
     @Inputs.data
     def set_data(self, dataset):
-        self.closeContext()
+        # self.closeContext()
         if dataset is None:
             self.sample = None
         else:
-            self.openContext(dataset.domain)  #Kaj je funkcija contexta
+            # self.openContext(dataset.domain)  # what is the function of context?
+            pass
 
         self.dataset = dataset
-        self.optionsBox.setDisabled(False)
-        self.commit.now() # Takoj poklici metodo commit
+        self.commit.now()
 
     def factor_analysis(self):
-        # Z izbranimi n_componentami izracunaj FA na self.dataset
-        result = FactorAnalysis(self.n_components).fit(self.dataset.X)
-        # Iz spremenljivke result (ki je instance nekega razreda) izlusci samo tabelo ki nas zanima
-        self.components = result.components_
+        # with chosen n_components and depending on the user-selected rotation, calculate the FA on self.dataset
+        if self.setting_for_rotation == 0:
+            result = FactorAnalysis(self.n_components).fit(self.dataset.X)
+        elif self.setting_for_rotation == 1:
+            result = FactorAnalysis(self.n_components, rotation="varimax").fit(self.dataset.X)
+        elif self.setting_for_rotation == 2:
+            result = FactorAnalysis(self.n_components, rotation="quartimax").fit(self.dataset.X)
+        else:
+            print("Error:")
 
-        # Pretvori tabelo nazaj v Orange.data.Table
+        # from result variable (instance of class) only extract the table we are interested in (components)
+        calculated_components = result.components_
+
+        # transform the table back to Orange.data.Table
         self.result = Table.from_numpy(Domain(self.dataset.domain.attributes),
-                                       self.components)
+                                       calculated_components)
 
     @gui.deferred
     def commit(self):
@@ -100,8 +179,9 @@ class OWFactorAnalysis(OWWidget):
             self.Outputs.sample.send(None)
         else:
             self.factor_analysis()
-            # Poslji self.result v Outputs channel.
+            # send self.result in Outputs channel
             self.Outputs.sample.send(self.result)
+            self.setup_plot()
 
 
 if __name__ == "__main__":
